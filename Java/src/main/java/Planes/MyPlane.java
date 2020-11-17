5110ec986ff71132a4cc2fe3d9dfd92761c324c5
Anastasiya Novik <nastiuchanoivk00@mail.ru>
2020-11-17 20:18:16 +0300

    new commit

---
+++ Java/src/main/java/Planes/MyPlane.java
@@ -0,0 +1,7 @@
+package Planes;
+
+public class MyPlane  extends Plane {
+    public MyPlane(String modelOfPlane, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(modelOfPlane, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+}
